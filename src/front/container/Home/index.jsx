1d48b702de3487818201d2a75df8a2ca68f7c064
oneshark <wfrancis0525@gmail.com>
2022-05-31 17:32:18 +0800

    目录结构优化

--- src/front/container/Home/index.jsx
+++ src/front/container/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './components/Banner'
 import Courses from './components/Courses'
+import Footer from './components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
       <Courses/>
+      <Footer/>
     </div>
   )
 }
