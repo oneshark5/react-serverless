01c6e8312759f8dc10c84d54d76e2899260e7261
oneshark <wfrancis0525@gmail.com>
2022-06-10 19:10:23 +0800

    添加路由

---
+++ src/admin/container/BasicSetting/index.jsx
@@ -0,0 +1,65 @@
+
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Button } from 'antd';
+// import styles from './style.module.scss'
+// import AreaList from './components/AreaList';
+// import { parseJsonByString } from '../../../common/utils';
+// import { getChangeSchemaAction } from './store/action';
+
+// // store中存取数据（把使用store的逻辑放在一起）
+// const useStore = () => {
+//   const dispatch = useDispatch()
+//   // 使用redux，采用useSelector拿到仓库的数据
+//   const schema = useSelector((state) => {
+//     return state.homeManagement.schema
+//   })
+//   // dispatch
+//   const changeSchema = (schema) => {
+//     // 调用dispatch
+//     dispatch(getChangeSchemaAction(schema))
+//   }
+//   return { schema, changeSchema }
+// }
+
+// const BasicSetting = () => {
+
+//   const { schema, changeSchema } = useStore()
+
+
+//   // 获取子组件AreaList的children
+//   const handleSaveBtnClick = () => {
+//     window.localStorage.schema = JSON.stringify(schema)
+//   }
+//   // 要重置的是children
+//   // 改变props，子组件跟着渲染就可以
+//   const handleResetBtnClick = () => {
+//     const newSchema = parseJsonByString(window.localStorage.schema, {})
+//     changeSchema(newSchema)//action
+//   }
+
+//   return (
+//     <div>
+//       <AreaList />
+//       <div className={styles.buttons}>
+//         <Button type="primary" onClick={handleSaveBtnClick}>
+//           保存基础配置
+//         </Button>
+//         <Button type="primary" className={styles.reset} onClick={handleResetBtnClick}>
+//           重置基础配置
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BasicSetting
+
+
+const BasicSetting = () => {
+  return (
+    <div>
+      'bbbbbb'
+    </div>
+  )
+}
+export default BasicSetting
--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -1,15 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, NavLink } from 'react-router-dom'
 import { Layout, Menu } from 'antd';
 import { Provider } from 'react-redux'
 import store from './store'
-import HomeManagement from './container/HomeManagement';
 import styles from './style.module.scss'
-
 import 'normalize.css' // 页面样式标准化
 import 'antd/dist/antd.css'
 import './style.scss';
-
+import AdminRouter from './container/AdminRouter';
 
 
 const { Header, Sider, Content } = Layout;
@@ -26,45 +25,54 @@ const useCollapsed = () => {
 const Wrapper = () => {
 
   const { collapsed, toggleCollapsed } = useCollapsed()
-  const handleHomePageRedirect = () => {window.location.href = "/"}
+  const handleHomePageRedirect = () => { window.location.href = "/" }
 
   return (
-    <Layout>
-      <Sider className={styles.sidebar} trigger={null} collapsible collapsed={collapsed}>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-        >
-          <Menu.Item key="admin-home">
-            <span className='iconfont'>&#xe69b;</span> 首页内容管理
-          </Menu.Item>
-          <Menu.Item
-            key="admin-back"
-            onClick={handleHomePageRedirect}>
-            <span className='iconfont' >&#xe7e5;</span> 返回用户页面
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className={styles.header}>
-          {
-            collapsed
-              ? <span className='iconfont' onClick={toggleCollapsed} >&#xe62c;</span>
-              : <span className='iconfont' onClick={toggleCollapsed} >&#xe629;</span>
-          }
-        </Header>
-        <Content className={styles.content}>
-        <HomeManagement/>
-        </Content>
+    <HashRouter>
+      <Layout>
+        <Sider className={styles.sidebar} trigger={null} collapsible collapsed={collapsed}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+          >
+            <Menu.Item key="admin-home">
+              <NavLink to='/'>
+                <span className='iconfont'>&#xe69b;</span> 首页内容管理
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item key="admin-setting">
+              <NavLink to='/setting'>
+              <span className='iconfont'>&#xe69b;</span> 基础内容配置
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item
+              key="admin-back"
+              onClick={handleHomePageRedirect}>
+              <span className='iconfont' >&#xe7e5;</span> 返回用户页面
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className={styles.header}>
+            {
+              collapsed
+                ? <span className='iconfont' onClick={toggleCollapsed} >&#xe62c;</span>
+                : <span className='iconfont' onClick={toggleCollapsed} >&#xe629;</span>
+            }
+          </Header>
+          <Content className={styles.content}>
+            <AdminRouter />
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </HashRouter>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-      <Wrapper/>
-    </Provider>
+  <Provider store={store}>
+    <Wrapper />
+  </Provider>
 );
