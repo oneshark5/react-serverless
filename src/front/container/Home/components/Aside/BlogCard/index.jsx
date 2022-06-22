696173a875326ce8d2fda4c834c540b302b0e802
oneshark <wfrancis0525@gmail.com>
2022-06-22 20:27:13 +0800

    Aside

--- src/front/container/Home/components/Aside/BlogCard/index.jsx
+++ src/front/container/Home/components/Aside/BlogCard/index.jsx
@@ -1,32 +1,25 @@
 import React from 'react'
-import styles from './style.module.scss'
+import Card from '../../Card';
+import styles from './index.scss'
 
 
 
 const Aside = ({ schema }) => {
   // 从Aside组件获取属性
-  // const { title, heightCard,  description, imageUrl, tags, icon, link,  } = schema
-  // console.log(schema);
+  const { title, heightCard,  description, imageUrl, tags, icon, link,  } = schema
+  console.log(schema);
 
   return (
-    <div className={styles.aside}>
-      {/* <ul className={styles.list}>
-        {
-          children.map((item, index) => {
-            const { attributes = {} } = item
-            const { heightCard, title, description, imageUrl, tags, link, icon } = attributes
-            return (
-              <li className={styles.item} key={index}>
-                <a className={styles.link} href={link} target="_blank" rel='noreferrer'>
-                  <h4 className={styles.title}>{title || '暂无标题'}</h4>
-                  <p className={styles.desc}>{description || '暂无描述'}</p>
-                </a>
-              </li>
-            )
-          })
-        }
-      </ul> */}
-    </div>
+    <Card className={styles.card}>
+      <p className={styles.text}>
+        晚上好<br />
+        我叫<span className={styles.color}>飞鸟</span>，<br />
+        欢迎来到
+        <br />
+        我的<span className={styles.color}>个人博客</span>。
+      </p>
+      <img src={imageUrl} className={styles.avatar} />
+    </Card>
   )
 }
 export default Aside
--- src/front/container/Home/index.jsx
+++ src/front/container/Home/index.jsx
@@ -3,7 +3,6 @@ import { Helmet } from "react-helmet";
 import Banner from './components/Banner'
 import Section from './components/Section'
 import Aside from './components/Aside'
-import Footer from './components/Footer'
 import styles from './index.module.scss'
 // import './global.custom.scss'
 
@@ -13,7 +12,7 @@ const { children = [], attributes = {} } = pageSchema
 const { title = '', poem = '', backgroundUrl = '' } = attributes
 
 
-const map = { Banner, Footer, Section, Aside }
+const map = { Section, Aside }
 
 const render = (item, index) => {
   const Component = map[item.name]
