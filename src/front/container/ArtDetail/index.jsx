45f313bda4f91559a7eb32fa088438e70b185793
oneshark <wfrancis0525@gmail.com>
2022-06-27 21:59:11 +0800

    artDetailRouter

--- src/front/container/ArtDetail/index.jsx
+++ src/front/container/ArtDetail/index.jsx
@@ -1,77 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../Layout'
 import ArtContent from './ArtContent'
+import { parseJsonByString } from '../../../common/utils'
+import { useSearchParams } from 'react-router-dom'
 
-
-
-const data = {
-  testContent: `
-  ## 2. BOM和DOM
-
-  ### 基础知识点
-  
-  * BOM
-    浏览器对象模型，它指的是把浏览器当做一个对象来对待，这个对象主要定义了与浏览器进行交互的方法和接口。
-  
-    * location
-  
-      提供了当前窗口中加载文档的信息，以及通常的导航功能。
-  
-    * history 对象
-      history对象主要用来操作浏览器URL的历史记录，可以通过参数向前，向后，或者向指定URL跳转
-  
-      - history.go() -- 前进或后退指定的页面数 history.go(num);
-      - history.back() -- 后退一页
-      - history.forward() -- 前进一页
-  
-    * Navigator
-      navigator 对象主要用来获取浏览器的属性，区分浏览器类型。属性较多，且兼容性比较复杂。
-  
-      - navigator.userAgent -- 返回用户代理头的字符串表示(就是包括浏览器版本信息等的字符串)
-      - navigator.cookieEnabled -- 返回浏览器是否支持(启用)cookie
-  
-    * screen
-      保存的纯粹是客户端能力信息，也就是浏览器窗口外面的客户端显示器的信息，比如像素宽度和像素高度
-  
-  * DOM
-    DOM 指的是文档对象模型，它指的是把文档当做一个对象，这个对象主要定义了处理网页内容的方法和接口。==DOM 表示由多层节点构成的文档，通过它开发者可以添加、删除和修改页面的各个部分。
-    ## 3. BOM和DOM
-
-    ### 基础知识点
-    
-    * BOM
-      浏览器对象模型，它指的是把浏览器当做一个对象来对待，这个对象主要定义了与浏览器进行交互的方法和接口。
-    
-      * location
-    
-        提供了当前窗口中加载文档的信息，以及通常的导航功能。
-    
-      * history 对象
-        history对象主要用来操作浏览器URL的历史记录，可以通过参数向前，向后，或者向指定URL跳转
-    
-        - history.go() -- 前进或后退指定的页面数 history.go(num);
-        - history.back() -- 后退一页
-        - history.forward() -- 前进一页
-    
-      * Navigator
-        navigator 对象主要用来获取浏览器的属性，区分浏览器类型。属性较多，且兼容性比较复杂。
-    
-        - navigator.userAgent -- 返回用户代理头的字符串表示(就是包括浏览器版本信息等的字符串)
-        - navigator.cookieEnabled -- 返回浏览器是否支持(启用)cookie
-    
-      * screen
-        保存的纯粹是客户端能力信息，也就是浏览器窗口外面的客户端显示器的信息，比如像素宽度和像素高度
-    
-    * DOM
-      DOM 指的是文档对象模型，它指的是把文档当做一个对象，这个对象主要定义了处理网页内容的方法和接口。==DOM 表示由多层节点构成的文档，通过它开发者可以添加、删除和修改页面的各个部分。
-      
-  `
-}
 
 export default function ArtDetail() {
+  // 获取id
+  const [searchParams, setSearchParams] = useSearchParams()
+  let id = searchParams.get('id')
+
+  // 根据id获取schema数据
+  const pageSchema = parseJsonByString(window.localStorage.schema, {})
+  const { children = [] } = pageSchema;
+  const childrenAbout = children.filter(element => (element.name === 'ArticleDetail'))
+  const content = childrenAbout[0].children.filter(item => item.id === Number(id))
+  const articleContent = content.at(-1).articleContent
+  
   return (
     <Layout title='Web Workers：在后台线程执行指定脚本'  >
-      <ArtContent content={data.testContent} />
+      <ArtContent content={articleContent} />
     </Layout>
   )
 }
--- src/front/container/Home/components/Section/index.jsx
+++ src/front/container/Home/components/Section/index.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import dayjs from 'dayjs'
 import Card from '../../../Card'
 import { Pagination } from 'antd';
 import './pagination.custom.scss';
 import styles from './style.module.scss'
-import { NavLink } from 'react-router-dom';
-
-
-
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const Section = ({ schema }) => {
   // 从后台获取属性
   const { children = [] } = schema
 
-  return (
+  // 定义路由
+  const navigate = useNavigate()
 
+  const linkToDetail = (id) => {
+    // navigate(`/artDetail?id=${id}`)
+    navigate(`/artDetail`)
+  }
+
+
+  return (
     <div className={styles.section} >
       {
         children.map((item, index) => {
           const { attributes = {} } = item
           const { title, description, tags, createTime, link } = attributes
           return (
-            <NavLink  key={index} to={link}>
-            <Card className={styles.card} >
-              <div className={styles.title}>{title}</div>
-              <p className={styles.description}>{description}</p>
-              <div className={styles.info}>
-                <span className={styles.date}>{dayjs(createTime).format('YYYY-MM-DD')}</span>
-                <span className={styles.tags}>{tags}</span>
-              </div>
-            </Card>
+            <NavLink key={index} to={{pathname:`/artDetail?id=${link}`}}>
+              <Card className={styles.card} >
+                <div className={styles.title}>{title}</div>
+                <p className={styles.description}>{description}</p>
+                <div className={styles.info}>
+                  <span className={styles.date}>{dayjs(createTime).format('YYYY-MM-DD')}</span>
+                  <span className={styles.tags}>{tags}</span>
+                </div>
+              </Card>
             </NavLink>
           )
         })
