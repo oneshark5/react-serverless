4ed548dbd66b1d3f48d961e30bc8faf116733ab6
oneshark <wfrancis0525@gmail.com>
2022-07-03 18:59:17 +0800

    feat nav hidden

--- src/front/container/Home/components/Banner/index.jsx
+++ src/front/container/Home/components/Banner/index.jsx
@@ -4,20 +4,33 @@ import styles from './style.module.scss'
 import { Drawer } from 'antd';
 import { MenuOutlined, HomeOutlined, BgColorsOutlined, SettingOutlined } from '@ant-design/icons';
 import { NavLink, useNavigate } from 'react-router-dom';
+import classNames from 'classnames'
+import { useEventListener } from 'ahooks';
 
 const Banner = ({ schema }) => {
   // 从后台获取属性
   const { children = [] } = schema
-
   // 路由，编程式导航;只需要在navigate()里添加要跳转的页面即可
   const navigate = useNavigate()
 
   // 移动端按钮
   const [visible, setVisible] = useState(false)
+  // 导航栏显示与隐藏
+  const [navShow, setNavShow] = useState(true)
+
+  // 引入ahooks实现导航栏的显示与隐藏
+  useEventListener(
+    'mousewheel',
+    event => {
+      event = event || window.event;
+      setNavShow(event.wheelDeltaY > 0);
+    },
+    { target: document.body }
+  );
 
   return (
     <div className='Nav'>
-      <nav className={styles.nav}>
+      <nav className={classNames(styles.nav, { [styles.hiddenNav]: !navShow })}>
         <div className={styles.navContent}>
 
           <div className={styles.homeBtn} onClick={() => navigate('/')}>
