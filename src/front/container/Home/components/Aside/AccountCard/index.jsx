36dad391e73393c6e23aab22a56e431b885e551e
oneshark <wfrancis0525@gmail.com>
2022-07-18 15:42:35 +0800

    accountAside

--- src/front/container/Home/components/Aside/AccountCard/index.jsx
+++ src/front/container/Home/components/Aside/AccountCard/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Card from '../../Card';
 import styles from './index.module.scss'
-import { GithubOutlined,IdcardOutlined,WechatOutlined,QqOutlined } from '@ant-design/icons';
+import { GithubOutlined, IdcardOutlined, WechatOutlined, QqOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import IcoBtn from './IcoBtn';
 
 // 配置数据：链接（GitHub、简历、微信、qq二维码）
 /**
@@ -15,23 +17,59 @@ title: "AccountCard"
  */
 
 // 飞鸟数据：isLink, link, ico, content 
+const imgStyle = { width: '120px', height: '120px' };
 
 const AccountCard = ({ schema }) => {
+  // 获取链接和二维码
+  const { link } = schema
+  const linkArr = link.split(',')
+  console.log(linkArr);
+
+  const accountData = [
+    {
+      isLink: true,
+      link: linkArr[0],
+      ico: <GithubOutlined />,
+      content: null
+    },
+    {
+      isLink: true,
+      link: linkArr[1],
+      ico: <IdcardOutlined />,
+      content: null
+    },
+    {
+      isLink: false,
+      link: '',
+      ico: <WechatOutlined />,
+      content: <img src={linkArr[2]} style={imgStyle} />
+    },
+    {
+      isLink: false,
+      link: '',
+      ico: <QqOutlined />,
+      content: <img src={linkArr[3]} style={imgStyle} />
+    },
+  ]
+  console.log(accountData);
+
+
+
+  //--=========== 生成图标 ==========--//
+
+
   // 显示二维码
-  const 
-  const { icon, link} = schema
-  const iconArr = icon.split(',')
-  console.log(iconArr);
-  
-  // 从Aside组件获取属性
-  const { imageUrl  } = schema
+  const [linkShow, setLinkShow] = useState(false)
 
   return (
     <Card className={styles.card}>
-      <GithubOutlined/>
-      <IdcardOutlined/>
-      <WechatOutlined/>
-      <QqOutlined/>
+      {
+        accountData.map(({ isLink, link, ico, content }, index) => (
+          <IcoBtn isLink={isLink} link={link} content={content} key={index}>
+            {ico}
+          </IcoBtn>
+        ))
+      }
     </Card>
   )
 }
