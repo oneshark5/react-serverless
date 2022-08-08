53a77cef7135a5aa96740b8e787ac7db2f6ee496
oneshark <wfrancis0525@gmail.com>
2022-08-08 20:20:49 +0800

    feat: add uuid

--- src/front/container/Home/components/Aside/MusicCard/index.jsx
+++ src/front/container/Home/components/Aside/MusicCard/index.jsx
@@ -7,8 +7,8 @@ const MusicCard = () => {
   return (
     <Card className={s.card}>
       <div>
-      <iframe frameborder="no" border="0" 
-        marginwidth="0" marginheight="0" 
+      <iframe frameBorder="no" border="0" 
+        marginWidth="0" marginHeight="0" 
         width="298px" height="52px" 
         src="//music.163.com/outchain/player?type=0&id=3136952023&auto=1&height=32"
         className={s.musicItem}
