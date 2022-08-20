8dcfe58471d802020183c6965768c338d53ce09c
oneshark <wfrancis0525@gmail.com>
2022-08-20 22:40:46 +0800

    feat: add drag flag

--- src/front/container/Home/components/Aside/MusicCard/index.jsx
+++ src/front/container/Home/components/Aside/MusicCard/index.jsx
@@ -6,14 +6,15 @@ const MusicCard = () => {
 
   return (
     <Card className={s.card}>
-      <div>
+      {/* <div>
       <iframe frameBorder="no" border="0" 
         marginWidth="0" marginHeight="0" 
         width="298px" height="52px" 
         src="//music.163.com/outchain/player?type=0&id=3136952023&auto=1&height=32"
         className={s.musicItem}
       />
-      </div>
+      </div> */}
+      <span className={s.dragFlag}>尝试拖拽一下🤚</span>
     </Card >
   );
 };
