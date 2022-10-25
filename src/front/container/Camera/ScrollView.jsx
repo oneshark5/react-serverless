dd3bf33e50214857c9ce62fd75ac80ed186ed3ab
oneshark <wfrancis0525@gmail.com>
2022-10-25 19:43:24 +0800

    scroll

--- src/front/container/Camera/ScrollView.jsx
+++ src/front/container/Camera/ScrollView.jsx
@@ -31,12 +31,16 @@ export default function ScrollView(props) {
   /* 判断滚动条是否到底部 */
   // scrollHeight 元素内容高度(包括由于溢出导致的视图中不可见内容。)
   // scrollTop 元素的内容顶部（卷起来的）到它的视口可见内容（的顶部）的距离
-  // offsetHeight 
+  // offsetHeight  获取 div 元素的高度，包含内边距（padding）和边框（border）
   const handerScrolltolower = (e) => {
     // console.log('到底就输出', scrolltolower)
     const { scrollHeight, scrollTop, offsetHeight } = e.target.scrollingElement
-    console.log('距数据最低端距离', offsetHeight - scrollTop + 178);
-    if (scrollHeight === scrollTop + offsetHeight) { /* 到达容器底部位置 */
+    console.log('距数据最低端距离', offsetHeight - scrollTop);
+    console.log(scrollHeight);
+    console.log(scrollTop);
+    console.log(offsetHeight);
+    console.log(scrollTop + offsetHeight);
+    if (scrollHeight < scrollTop + offsetHeight+10) { /* 到达容器底部位置 */
       scrolltolower && scrolltolower()
     }
   }
